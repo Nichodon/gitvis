adc3eb7895ac1822a72ed12e18576fcd7e999ac7
Nichodon <brandonsyc.2222@yahoo.com>
2017-01-06 16:41:43 -0800

    Displays HEAD

--- gitvis.py
+++ gitvis.py
@@ -1,5 +1,23 @@
 from Tkinter import *
 
+with open('.git/HEAD') as x:
+    f = x.read()
+
+print f
+
+f = f[5:].rstrip()
+
+with open('.git/' + f) as x:
+    g = x.read()
+
+print g
+
 tk = Tk()
+
+tk.minsize(width=750, height=500)
+tk.maxsize(width=750, height=500)
+
+t = Message(tk, text='[' + f + ']: ' + g, width=500)
+t.grid(row=0, column=0)
 
 mainloop()
