d7c73d3f55e6a6ee26afb217e76036d0b0a61b3c
Nichodon <brandonsyc.2222@yahoo.com>
2017-01-20 16:44:47 -0800

    Display last two commits

--- gitvis.py
+++ gitvis.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from zlib import *
 
 
 def mark(event):
@@ -7,6 +8,12 @@ def mark(event):
 
 def dragto(event):
     event.widget.scan_dragto(event.x, event.y, gain=1)
+
+
+def create_node(x_pos, y_pos, data):
+    c1.create_oval(x_pos - 30, y_pos - 25, x_pos + 30, y_pos + 25, outline='red')
+    c1.create_text(x_pos, y_pos, text=data)
+    c1.create_line(x_pos - 30, y_pos, x_pos - 70, y_pos, arrow=LAST)
 
 with open('.git/HEAD') as x:
     f = x.read()
@@ -38,12 +45,20 @@ c1.config(xscrollcommand=sc1.set, yscrollcommand=sc2.set)
 c1.grid(row=0, column=0)
 c1.bind('<ButtonPress-1>', mark)
 c1.bind('<B1-Motion>', dragto)
-c1.create_oval(50, 50, 100, 100, outline='red')
+create_node(200, 50, g[:7])
 
 lf2 = LabelFrame(tk, text='Information')
 lf2.grid(row=0, column=1)
 
 l1 = Label(lf2, text='[' + f + ']: ' + g[:7])
 l1.grid(row=0, column=0)
+
+with open('.git/objects/' + g[:2] + '/' + g[2:].rstrip()) as x:
+    h = x.read()
+
+h = decompress(h)
+
+lines = h.split('\x00')
+create_node(100, 50, lines[1].split('parent ')[1][:7])
 
 mainloop()
